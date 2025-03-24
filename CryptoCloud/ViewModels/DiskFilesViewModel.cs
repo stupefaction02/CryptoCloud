187236015ff49c221dece2d3e5a051b56ed392ae
Ivan <woundedsoul1994@gmail.com>
2025-03-24 16:20:14 +0500

    doesn't work

--- CryptoCloud/ViewModels/DiskFilesViewModel.cs
+++ CryptoCloud/ViewModels/DiskFilesViewModel.cs
@@ -31,6 +31,27 @@ namespace CryptoCloud.ViewModels
         public string Type { get; set; }
 
         public DiskFileItemModel Model { get; set; }
+
+        public DiskGroup Group { get; set; } = DiskGroup.Default;
+    }
+
+    public class DiskGroup
+    {
+        public static readonly DiskGroup Default = new DiskGroup { Key = "allFiles", Name = "Все файлы" };
+
+        public string Name { get; set; }
+
+        public string Key { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is DiskGroup otherGroup)
+            {
+                return otherGroup.Key == this.Key;
+            }
+
+            return false;
+        }
     }
 
     public class DiskFilesViewModel : BaseViewModel
@@ -40,6 +61,8 @@ namespace CryptoCloud.ViewModels
         public string DiskOwner { get; set; }
 
         //ObservableGroupedCollection
+
+        public ObservableGroupedCollection<DiskGroup, DiskItemViewModel> Groups { get; private set; } = new();
 
         public DiskFilesViewModel()
         {
@@ -53,6 +76,14 @@ namespace CryptoCloud.ViewModels
                 new DiskItemViewModel { Type = "file", Model = new DiskFolderModel { Name = "ABOBA.png", ModificationDate = "28.13.2023 11:32" } },
                 new DiskItemViewModel { Type = "file", Model = new DiskFolderModel { Name = "x-file (1).png", ModificationDate = "89.01.2123 29:32" } },
             };
+
+            var a = Files
+                .GroupBy(static c => c.Group)
+                .OrderBy(static g => g.Key).ToList();
+
+            Groups = new ObservableGroupedCollection<DiskGroup, DiskItemViewModel>(
+                a
+            );
         }
     }
 }
