4e2a0ea0905827f47bcf539103d67b22f6d85b74
Ivan <woundedsoul1994@gmail.com>
2025-03-24 15:02:22 +0500

    upd.

--- CryptoCloud/ViewModels/DiskFilesViewModel.cs
+++ CryptoCloud/ViewModels/DiskFilesViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.Collections;
+using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -8,12 +9,50 @@ using System.Threading.Tasks;
 
 namespace CryptoCloud.ViewModels
 {
+    public class DiskFileItemModel
+    {
+        public string Name { get; set; }
+
+        public string ModificationDate { get; set; }
+    }
+
+    public class DiskFolderModel : DiskFileItemModel
+    {
+
+    }
+
+    public class DiskFileModel : DiskFileItemModel
+    {
+
+    }
+
+    public class DiskItemViewModel : ObservableObject
+    {
+        public string Type { get; set; }
+
+        public DiskFileItemModel Model { get; set; }
+    }
+
     public class DiskFilesViewModel : BaseViewModel
     {
-        //public ObservableCollection<> Files { get; set; }
+        public ObservableCollection<DiskItemViewModel> Files { get; set; }
 
         public string DiskOwner { get; set; }
 
         //ObservableGroupedCollection
+
+        public DiskFilesViewModel()
+        {
+            Files = new ObservableCollection<DiskItemViewModel>
+            {
+                new DiskItemViewModel { Type = "folder", Model = new DiskFolderModel { Name = "Папка 1", ModificationDate = "24.03.2025 9:32" } },
+                new DiskItemViewModel { Type = "folder", Model = new DiskFolderModel { Name = "Папка 123", ModificationDate = "14.02.2015 13:32" } },
+                new DiskItemViewModel { Type = "folder", Model = new DiskFolderModel { Name = "Папка - copy (123)", ModificationDate = "8.03.2023 11:32" } },
+                new DiskItemViewModel { Type = "file", Model = new DiskFolderModel { Name = "file.png", ModificationDate = "8.03.2023 11:32" } },
+                new DiskItemViewModel { Type = "file", Model = new DiskFolderModel { Name = "x-file.png", ModificationDate = "8.03.2023 11:32" } },
+                new DiskItemViewModel { Type = "file", Model = new DiskFolderModel { Name = "ABOBA.png", ModificationDate = "28.13.2023 11:32" } },
+                new DiskItemViewModel { Type = "file", Model = new DiskFolderModel { Name = "x-file (1).png", ModificationDate = "89.01.2123 29:32" } },
+            };
+        }
     }
 }
